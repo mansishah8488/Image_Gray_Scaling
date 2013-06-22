7571155ce71ec4cd8ba090ba1b7d95f88ced2967
k comp <kcomp@ks-Mac-mini.local>
2013-06-22 18:44:34 +0530

    Initial Commit

---
+++ Image_Gray_Scaling/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Image_Gray_Scaling
+//
+//  Created by k comp on 22/06/13.
+//  Copyright (c) 2013 k comp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Image_Gray_Scaling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Image_Gray_Scaling
+//
+//  Created by k comp on 22/06/13.
+//  Copyright (c) 2013 k comp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
